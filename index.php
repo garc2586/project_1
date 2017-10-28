c6e6429f7955cdb3833c559e40e14d620c39b162
Carlos Garcia <cargarcia@csumb.edu>
2017-10-28 00:06:43 +0000

    added index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        <style type="text/css">
+            h1{
+                text-align:center;
+            }
+            #list{
+                margin-left:275px;
+            }
+        </style>
+        <h1>Project 1</h1>
+        
+        <div id=list>
+            <a href="/Project1/project1.php">Assignments</a>
+        </div>
+        <div></div>
+    </body>
+</html>
